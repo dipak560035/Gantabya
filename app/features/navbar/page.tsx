e7d4f2b4ff7a9005a3d6a8679d9e7e431f16a70f
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-05-31 21:38:21 +0545

    Update page.tsx

--- app/features/navbar/page.tsx
+++ app/features/navbar/page.tsx
@@ -5,13 +5,6 @@ export default function Page() {
     <div className="bg-[#111111] text-white h-25">
         <nav className="flex items-center justify-between px-10 py-6">
         
-            <div className="flex items-center gap-2">
-            <div className="w-12 h-12 bg-green-500 rounded-full flex items-center justify-center font-bold text-black">G</div>
-            <span className="text-xl font-bold">gantabya</span>
-            </div>
-
-            <div className="flex items-center gap-8 text-gray-300">
-            <Link href="/" className="text-green-500 font-medium">Home</Link>
             <Link href="/" className="hover:text-white transition">Our Bikes</Link>
             <Link href="/" className="hover:text-white transition">Technology</Link>
             <Link href="/" className="hover:text-white transition">About Us</Link>
