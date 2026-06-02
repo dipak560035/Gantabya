6e36f31f1512389b7e08362288085139a6abf05a
Dipak Sah <92229078+dipak560035@users.noreply.github.com>
2026-06-02 11:22:34 +0545

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -84,16 +84,7 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-footer"
-      className={cn(
-        "flex items-center rounded-b-xl border-t bg-muted/50 p-4 group-data-[size=sm]/card:p-3",
-        className
-      )}
-      {...props}
-    />
-  )
-}
-
-
+ 
 
 
 
